a4242a391a1413b3df77c70474e5d409e7c0d54b
hunman89 <hunman89@gmail.com>
2021-12-08 18:37:45 +0900

    add. get blocks and create block request

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/hunman89/nomadcoin/blockchain"
+	"github.com/hunman89/nomadcoin/utils"
 )
 
 const port string = ":4000"
@@ -21,6 +24,10 @@ type URLDescription struct {
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
@@ -40,8 +47,22 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
