6cc1e4952e6e5a5047122fdca4a16c7cc9c97b53
hunman89 <hunman89@gmail.com>
2021-12-09 11:45:04 +0900

    add. cli with flag

--- main.go
+++ main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to nomadcoin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer: 	Start the HTML Explorer\n")
-	fmt.Printf("rest: 		Start the REST API (recommanded)\n\n")
-	os.Exit(0)
-}
+import "github.com/hunman89/nomadcoin/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		fmt.Println("Start REST API")
-	default:
-		usage()
-	}
+	cli.Start()
 }
