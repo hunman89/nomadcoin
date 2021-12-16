9e043b0df9e6c8b5ed6c66df98d4b0862892e99e
hunman89 <hunman89@gmail.com>
2021-12-16 10:30:47 +0900

    add. buffered channel

--- main.go
+++ main.go
@@ -5,27 +5,29 @@ import (
 	"time"
 )
 
-func countToTen(c chan<- int) {
+func send(c chan<- int) {
 	for i := range [10]int{} {
+		fmt.Printf(">> sending %d <<\n", i)
 		c <- i
-		time.Sleep(1 * time.Second)
+		fmt.Printf(">> sent %d <<\n", i)
 	}
 	close(c)
 }
 
 func receive(c <-chan int) {
 	for {
+		time.Sleep(10 * time.Second)
 		a, ok := <-c
 		if !ok {
 			fmt.Println("we are done")
 			break
 		}
-		fmt.Println(a)
+		fmt.Printf("|| received %d ||\n", a)
 	}
 }
 
 func main() {
-	c := make(chan int)
-	go countToTen(c)
+	c := make(chan int, 10)
+	go send(c)
 	receive(c)
 }
