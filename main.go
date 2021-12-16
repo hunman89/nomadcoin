084fe33cf649d12e96dd7fef5e4fd0f749bf4045
hunman89 <hunman89@gmail.com>
2021-12-16 10:15:34 +0900

    add. close channel, send and receive only channel

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"time"
 )
 
-func countToTen(c chan int) {
+func countToTen(c chan<- int) {
 	for i := range [10]int{} {
 		c <- i
 		time.Sleep(1 * time.Second)
+	}
+	close(c)
+}
+
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("we are done")
+			break
+		}
+		fmt.Println(a)
 	}
 }
 
 func main() {
 	c := make(chan int)
 	go countToTen(c)
-	for {
-		a := <-c
-		fmt.Println(a)
-	}
+	receive(c)
 }
