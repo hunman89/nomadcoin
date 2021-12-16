629df51e0283e1389af85b5820a5c3ff7d499ce6
hunman89 <hunman89@gmail.com>
2021-12-16 10:02:08 +0900

    add. go routines and channel

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"github.com/hunman89/nomadcoin/cli"
-	"github.com/hunman89/nomadcoin/db"
+	"fmt"
+	"time"
 )
 
+func countToTen(c chan int) {
+	for i := range [10]int{} {
+		c <- i
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	c := make(chan int)
+	go countToTen(c)
+	for {
+		a := <-c
+		fmt.Println(a)
+	}
 }
