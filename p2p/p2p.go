f7b3c5a4c0a508ede23bc9704c879fbba17f50bf
hunman89 <hunman89@gmail.com>
2021-12-22 10:48:08 +0900

    add. message to request all blocks

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -11,17 +11,20 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+
 	openPort := r.URL.Query().Get("openPort")
 	ip := utils.Splitter(r.RemoteAddr, ":", 0)
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return openPort != "" && ip != ""
 	}
+	fmt.Printf("%s wants an upgrade\n", openPort)
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 	initPeer(conn, ip, openPort)
 }
 
 func AddPeer(address, port, openPort string) {
+	fmt.Printf("%s wants to connect to port %s\n", openPort, port)
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	p := initPeer(conn, address, port)
