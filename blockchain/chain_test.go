f49e0195c116b5c95b3a4d5cd3117cd4c87407b9
hunman89 <hunman89@gmail.com>
2021-12-26 21:49:58 +0900

    add. test find tx

--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -57,28 +57,87 @@ func TestBlockchain(t *testing.T) {
 }
 
 func TestBlocks(t *testing.T) {
-	fakeBlocks := 0
+	blocks := []*Block{
+		{PrevHash: "x"}, {PrevHash: ""},
+	}
+	fakeBlock := 0
 	dbStorage = fakeDB{
 		fakeFindBlock: func() []byte {
-			var b *Block
-			if fakeBlocks == 0 {
-				b = &Block{
-					Height:   2,
-					PrevHash: "x",
-				}
-			}
-			if fakeBlocks == 1 {
-				b = &Block{
-					Height: 1,
-				}
-			}
-			fakeBlocks++
-			return utils.ToBytes(b)
+			defer func() {
+				fakeBlock++
+			}()
+			return utils.ToBytes(blocks[fakeBlock])
 		},
 	}
 	bc := &blockchain{}
-	blocks := Blocks(bc)
-	if reflect.TypeOf(blocks) != reflect.TypeOf([]*Block{}) {
+	blocksResult := Blocks(bc)
+	if reflect.TypeOf(blocksResult) != reflect.TypeOf([]*Block{}) {
 		t.Error("Blocks() should return a slice of blocks")
+	}
+}
+
+func TestFindTx(t *testing.T) {
+	t.Run("Tx not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height:       2,
+					Transactions: []*Tx{},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx != nil {
+			t.Error("Tx should be not found.")
+		}
+	})
+	t.Run("Tx should be found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{
+					Height: 2,
+					Transactions: []*Tx{
+						{ID: "test"},
+					},
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		tx := FindTx(&blockchain{NewestHash: "x"}, "test")
+		if tx == nil {
+			t.Error("Tx should be found.")
+		}
+	})
+}
+
+func TestGetDifficulty(t *testing.T) {
+	blocks := []*Block{
+		{PrevHash: "x"}, {PrevHash: "x"}, {PrevHash: "x"}, {PrevHash: "x"}, {PrevHash: ""},
+	}
+	fakeBlock := 0
+	dbStorage = fakeDB{
+		fakeFindBlock: func() []byte {
+			defer func() {
+				fakeBlock++
+			}()
+			return utils.ToBytes(blocks[fakeBlock])
+		},
+	}
+	type test struct {
+		height int
+		want   int
+	}
+	tests := []test{
+		{height: 0, want: defaultDifficulty},
+		{height: 2, want: defaultDifficulty},
+		{height: 5, want: 3},
+	}
+	for _, tc := range tests {
+		bc := &blockchain{Height: tc.height, CurrentDifficulty: defaultDifficulty}
+		d := getDifficulty(bc)
+		if d != tc.want {
+			t.Errorf("getDifficulty() should return %d got %d", d, tc.want)
+		}
 	}
 }
